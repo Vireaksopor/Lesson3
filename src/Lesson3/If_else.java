31e677053898c67f261649330aad793ee4c2b1ec
Vireaksopor <Vireaksopor@gmail.com>
2021-08-10 21:24:21 +0700

    Initial commit

---
+++ src/Lesson3/If_else.java
@@ -0,0 +1,19 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class If_else {
+    c
+        Scanner cin = new Scanner(System.in);
+        String username =" ";
+        String passwork =" ";
+
+        if(username.equals("admin") && passwork.equals("123")){
+            System.out.println("login  Successfully!!");
+        }
+        else{
+            System.err.println("login faild");
+
+        }
+    }
+}
---
+++ src/Lesson3/break_continue.java
@@ -0,0 +1,12 @@
+package Lesson3;
+public class break_continue {
+    public static void main(String[] args){
+        for (int i=0 ; i<10 ; i++){
+            System.out.println("now i is: "+i);
+            if (i==5){
+                break;
+
+            }
+        }
+    }
+}
